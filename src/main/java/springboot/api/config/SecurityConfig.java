ef659d75b886ffa56dd392abc9e9d3b7e5ff0f4b
Luis Munar <luis.emunarr@gmail.com>
2024-08-25 09:28:50 -0500

    Video 201 ready

--- src/main/java/springboot/api/config/SecurityConfig.java
+++ src/main/java/springboot/api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package springboot.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,7 +19,13 @@ public class SecurityConfig {
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http.authorizeHttpRequests(
-      (auth) -> auth.requestMatchers("/users/**").permitAll().anyRequest().authenticated()
+      (auth) -> auth
+        .requestMatchers(HttpMethod.POST ,"/users/**")
+        .permitAll()
+        .requestMatchers(HttpMethod.GET ,"/users")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
     )
       .csrf(config -> config.disable())
       .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
